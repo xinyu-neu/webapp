215dd05c9db5a82a57e02f7de75a82deba6317b4
xinyu-qiu <113216096+xinyu-qiu@users.noreply.github.com>
2023-02-20 22:23:26 -0800

    add packer

--- packer/setup.sh
+++ packer/setup.sh
@@ -11,7 +11,7 @@ sudo mkdir /opt/deployment
 sudo mkdir /var/log/app
 
 sudo chown -R $USER:$USER /opt/deployment
-sudo chown -R $USER:$USER /var/log/appy
+sudo chown -R $USER:$USER /var/log/app
 
 sudo wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.rpm
 sudo rpm -ivh jdk-17_linux-x64_bin.rpm
